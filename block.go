eb0e1b97bf27fc3147d8f73fb73506f7a400c7c6
5pecia1 <pdpxpd@gmail.com>
2018-06-19 19:40:53 +0900

    part 2 pow

--- block.go
+++ block.go
@@ -1,38 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
+// Block keeps block headers
 type Block struct {
-	TimeStamp     int64
+	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
-func (b *Block) SetHash() {
-	timeStamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timeStamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
+// NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{
-		time.Now().Unix(),
-		[]byte(data),
-		prevBlockHash,
-		[]byte{},
-	}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
+// NewGenesisBlock creates and returns genesis Block
 func NewGenesisBlock() *Block {
-	return NewBlock("genesis Block", []byte{})
+	return NewBlock("Genesis Block", []byte{})
 }
--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	bc := NewBlockchain()
 
-	bc.Addblock("A한테 B가 100BTC 보냄")
-	bc.Addblock("C -> D 100BTC")
-	bc.Addblock("C -> D 100BTC")
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
-		fmt.Println("Prev ", block.PrevBlockHash)
-		fmt.Println("Data ", block.Data)
-		fmt.Println("Hash ", block.Hash)
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
